02310b510516b75222fb48c259e840bae84265e8
athikapriya <athikapriya1997@gmail.com>
2025-08-11 21:59:25 +0600

    Add Area of shapes.py

---
+++ Area of shapes.py
@@ -0,0 +1,14 @@
+base = float(input("enter base : "))
+height = float(input("enter height : "))
+radius = float(input("enter radius : "))
+pi = 3.1416
+
+square = base * height
+Triangle = 1/2 * (base * height)
+Circle = 2 * pi * radius
+rhombus = (base * height) / 2
+
+print("The Area of Square : ", square ,)
+print("The Area of Triangle : ", Triangle,)
+print("The Area of Circle :" , Circle ,)
+print("The Area of rhombus : ", rhombus)
