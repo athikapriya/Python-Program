c1d809db8ef88266ad0fe268dbb73ce57e7ad20a
athikapriya <athikapriya1997@gmail.com>
2025-08-11 21:02:09 +0600

    Add Numerical Operations.py

---
+++ Numerical Operations.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+mod = num1 % num2
+floor = num1 // num2
+exponent = num1 ** num2
+
+print("The sum is : ", sum,)
+print("The sub is : ", sub,)
+print("The mul is : ", mul,)
+print("The div is : ", div,)
+print("The mod is : ", mod,)
+print("The floor is : ", floor,)
+print("The exponent is : ", exponent,)
