9812a70d41aa52d68babac6d87a76f2f156fac31
athikapriya <athikapriya1997@gmail.com>
2025-08-11 20:50:26 +0600

    Add Variables and Data Types.py

---
+++ Variables and Data Types.py
@@ -0,0 +1,15 @@
+name = "Athika Priya"
+age = 20
+university = "Bangladesh University Of Professioanls"
+
+print("My name is " + name + ".") # string can be added by "+" sign
+print("I am" ,age , "years old.") # numbers should be added by ", ,"
+print("My University : " + university +  ".") # string
+
+# Using Backslash
+print("I am " + name + ". \nI am ",age," years old. \nI am currently studying at " + university + "."  )
+
+'''
+where " \n " specify a new line
+" \t "  specify 4 spaces
+'''
