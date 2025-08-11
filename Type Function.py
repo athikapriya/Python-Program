f41e3ee3e8f3bc673cd7d9745db692e91744c83b
athikapriya <athikapriya1997@gmail.com>
2025-08-11 22:22:00 +0600

    Add Type Function.py

---
+++ Type Function.py
@@ -0,0 +1,13 @@
+num1 = 20
+num2 = 10.5
+name = "Athika"
+mobile = "017777777"
+
+print(type(num1))
+print(type(num2))
+print(type(name))
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print("Athika Priya ." , end = " ")
+print("mobile number is "+ mobile)
+
