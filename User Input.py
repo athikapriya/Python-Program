fa6f4e17658728ac2dd0154d474c4eb733532f6c
athikapriya <athikapriya1997@gmail.com>
2025-08-11 21:02:09 +0600

    Add User Input.py

---
+++ User Input.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+height = input("How tall are you? ")
+
+print("My name is " + name + ".")
+print("I am ", age, "years old.")
+print("I am ", height, "meters tall.")
