24a6f3dd7ec002c538b2021b9689efb9dc15bec2
athikapriya <athikapriya1997@gmail.com>
2025-08-13 00:11:31 +0600

    Add Range Function.py

---
+++ Range Function.py
@@ -0,0 +1,9 @@
+num = list(range(10)) # shows 0 to 10
+print(num)
+print(num[3]) # shows index no 3
+
+num = list(range(4, 12)) # starts from 4, ends with the last digit before 12
+print(num)
+
+num = list(range(0, 31, 5)) # starting number, ending number, interval
+print(num)
