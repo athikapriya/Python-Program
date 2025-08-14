91ad82700fe3691f3919ccf5b1e36b0e561101bf
athikapriya <athikapriya1997@gmail.com>
2025-08-14 23:05:50 +0600

    Add Lambda function.py

---
+++ Lambda function.py
@@ -0,0 +1,10 @@
+# A function without name(Anonymous Function)
+# Not powerful as named function
+# It can work with single expression/single line of code
+
+'''syntax
+  lamda parameter : expression value
+'''
+
+result = (lambda a,b : a*a + 2*a*b + b*b) (2,3)
+print(result)
