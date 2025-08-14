7331bff91caf6bf69db537c19b20d075e1b7a77f
athikapriya <athikapriya1997@gmail.com>
2025-08-14 22:34:14 +0600

    Add xargs and xxargs.py

---
+++ xargs and xxargs.py
@@ -0,0 +1,23 @@
+### xargs
+def student(*details):
+    print(details[1])
+
+student("Athika", 20, 3.56)
+student("Rittika", 22, 3.00)
+
+# addition
+def add(*numbers):
+    sum = 0
+    for num in numbers:
+        sum = sum + num
+    print(sum)
+
+add(10, 20, 30)
+
+
+### xxargs
+def info(**detail_info):
+    print(detail_info)
+    print(detail_info["name"])
+
+info(id = 22, name = 'Rittika')
