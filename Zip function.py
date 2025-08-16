1a68ed1b838cd3cba5710633787585004eed11c3
athikapriya <athikapriya1997@gmail.com>
2025-08-16 13:58:48 +0600

    Add Zip function.py

---
+++ Zip function.py
@@ -0,0 +1,9 @@
+name = ["Athika", "Rittika", "Priya", "Sabitri"]
+roll = [1, 2, 3, 4]
+marks = [44, 35, 82, 78]
+
+print(list(zip(name, roll, marks)))
+
+result = list(zip(name, roll, marks, "ABAB")) # take value one by one
+print(result)
+
