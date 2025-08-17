cde4981d4a56e7cc3f60b3626ed53f18fe6e449f
athikapriya <athikapriya1997@gmail.com>
2025-08-17 17:18:11 +0600

    Data types added

--- Python Basics/Variables and Data Types.py
+++ Python Basics/Variables and Data Types.py
@@ -13,3 +13,72 @@ print("I am " + name + ". \nI am ",age," years old. \nI am currently studying at
 where " \n " specify a new line
 " \t "  specify 4 spaces
 '''
+
+# Text type : str
+print("\n=== Text Data types ===")
+name = "Athika Priya"
+print(type(name))
+
+# Numeric type : int, float, complex
+print("\n=== Numeric Data types ===")
+num1 = 2
+num2 = 3.5
+num3 = 2j
+print(type(num1))
+print(type(num2))
+print(type(num3))
+
+# Sequence type : list, tuple, range
+print("\n=== Sequence Data types ===")
+list = ["apple", "banana", "cherry"]
+tuple = ("apple", "banana", "cherry")
+range = range(5)
+print(type(list))
+print(type(tuple))
+print(type(range))
+
+# Mapping type : dict
+print("\n=== Mapping Data types ===")
+dict = {
+    "name" : "Athika Priya",
+    101 : "Section A",
+    "numbers" : (1, 2, 3, 4), # tuple as value
+    "skills" : ["python", "Java"], # list as value
+    "extra" : {
+        "a" : 1,
+        "b" : 2,
+    } # dict as value
+}
+print(type(dict))
+
+# Set type : set, frozenset
+print("\n=== Set type Data types ===")
+set ={ "apple", "banana", "cherry" }
+print(type(set))
+
+frozenset = frozenset({"apple", "banana", "cherry"})
+print(type(frozenset))
+
+# Boolean type : bool
+print("\n=== Boolean Data types ===")
+bool = False # True or False should be start with Capital letter
+print(type(bool))
+
+# Binary type : byte, bytearray, memoryview
+print("\n=== Binary Data types ===")
+binary = b"hello" # add 'b' in front of the str
+print('\n',binary)
+print(type(binary))
+
+bytearray = bytearray(5)
+print('\n',bytearray)
+print(type(bytearray))
+
+memoryview = memoryview(bytes(5))
+print("\n",memoryview)
+print(type(memoryview))
+
+# None type : none
+print("\n=== None type Data types ===")
+x = None
+print(type(x))
