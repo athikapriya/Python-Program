9d985181442cb9d795fa0239911adf9be07d5c3a
athikapriya <athikapriya1997@gmail.com>
2025-08-29 19:34:50 +0600

    while loop updated

--- Python Basics/while loop.py
+++ Python Basics/while loop.py
@@ -1,5 +1,5 @@
 # sum of n numbers
-n = int(input("Enter the number : "))
+n = int(input("Enter the number n : "))
 sum= 0
 i = 1
 while i <= n:
@@ -7,3 +7,12 @@ while i <= n:
     i = i + 1
 
 print(sum)
+
+
+# square
+count = 0
+while count < 5:
+    num = int(input("Enter the number for square: "))
+    square = num * num
+    print(square)
+    count += 1
