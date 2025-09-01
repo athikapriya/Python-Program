15839ffecc5f8f3922a2271284029ffb8277e69e
athikapriya <athikapriya1997@gmail.com>
2025-09-01 22:11:23 +0600

    Types of Inheritance.py updated

--- OOP/Types of Inheritance.py
+++ OOP/Types of Inheritance.py
@@ -4,18 +4,38 @@
 3. Multiple Inheritance
 '''
 
-
+# multilevel inheritance
 class A:
-    def display(self):
+    def display1(self):
         print("I am from class A")
 
-class B:
-    def display(self):
+class B(A):
+    def display2(self):
         print("I am from class B")
 
-class C(A, B):
-    pass
+class C(B):
+    def display3(self):
+        super().display1()
+        super().display2()
+        print("I am from class C")
 
 obj = C()
 print(isinstance(obj, B))
-obj.display()
+obj.display3()
+
+# multiple inheritance
+class a :
+    def display(self):
+        print("I am from class a")
+
+class b:
+    def display(self):
+        print("I am from class b")
+
+class c(a, b):
+    # def display(self):
+    #     print("I am from class c") ### this is show the class c for priority
+    pass # now result will show from class a, as i choose (a, then b)
+
+myobj = c()
+myobj.display()
